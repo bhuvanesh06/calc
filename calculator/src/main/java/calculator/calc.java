0b748f150bd2f94cfaa7da5f92d239d265956aa5
bhuvan <bhuvancool6493@gmail.com>
2016-08-11 13:27:20 +0530

    multi try

--- calculator/src/main/java/calculator/calc.java
+++ calculator/src/main/java/calculator/calc.java
@@ -11,11 +11,14 @@ public class calc {
 		double result = a - b;
 		return result;
 	}
-	
+
 	public double div(double a, double b) {
 		double result = a / b;
 		return result;
 	}
-	
-	
+
+	public double mul(double a, double b) {
+		double result = a * b;
+		return result;
+	}
 }
